fb893cd7522dc486c4790e9ab5baf1c3596724ba
Kimara5 <krisariasb@gmail.com>
2021-06-10 09:19:37 +0000

    Added 102 to new file

--- new-file.sh
+++ new-file.sh
@@ -1,1 +1,11 @@
-hello v1.0.2
+Pod::Spec.new do |spec|
+  spec.name         = 'MyFramework'
+  spec.version      = '1.0.1'
+  spec.license      = { :type => 'MIT' }
+  spec.homepage     = 'https://github.com/Kimara5/MyXCFramework'
+  spec.authors      = { 'Kimara5' => 'krisariasb@gmail.com' }
+  spec.summary      = 'MyFramework to test this is.'
+  spec.source       = { :git => 'https://github.com/Kimara5/MyXCFramework.git', :tag => spec.version }
+  spec.source_files = 'MyFramework.xcframework'
+  spec.framework    = 'SystemConfiguration'
+end
