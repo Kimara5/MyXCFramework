56fcdf68bcae98812a147a33574d3916179883df
Kimara5 <krisariasb@gmail.com>
2021-06-10 09:38:31 +0000

    Added 1.0.3 to new file

--- new-file.sh
+++ new-file.sh
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
   spec.name         = 'MyFramework'
-  spec.version      = '1.0.1'
+  spec.version      = '1.0.2'
   spec.license      = { :type => 'MIT' }
   spec.homepage     = 'https://github.com/Kimara5/MyXCFramework'
   spec.authors      = { 'Kimara5' => 'krisariasb@gmail.com' }
